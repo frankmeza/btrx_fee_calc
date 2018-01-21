853e0068af9bad1962f695079588da3d6f368e6b
Frank Meza <frankmezapdx@gmail.com>
2018-01-21 14:34:11 -0800

    create recordedPrice structs from histData csv

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
+	"log"
 	"strings"
 )
 
-// type recordedPrice struct {
-// }
+// recordedPrice is the market value of BTC on a given day
+type recordedPrice struct {
+	Date     string
+	LowPrice string
+}
+
+// recordedFee is the fee paid on a given day
+type recordedFee struct {
+	Date string
+	Fee  string
+}
 
 // createCsvReader for use with BTRX, HistDataBTC
 func createCsvReader(csvDataString string) *csv.Reader {
@@ -15,10 +26,32 @@ func createCsvReader(csvDataString string) *csv.Reader {
 	return r
 }
 
-// process getHistDataLowWithDate :: []string -> RecordedPrice{}
-// func getHistDataLowWithDate(histData []string) recordedPrice {
-// }
+// recordedPrice functions BEGIN
+
+func createAndSaveRecordedPrice(row []string) recordedPrice {
+	// get date, lowPrice values from row, create recordedPrice
+	record := recordedPrice{Date: row[0], LowPrice: row[3]}
+	return record
+}
+
+// process getHistDataLowWithDate :: []string -> []recordedPrice
+func processHistData(histData [][]string) []recordedPrice {
+	var dataContainer []recordedPrice
+	for _, row := range histData {
+		record := createAndSaveRecordedPrice(row)
+		dataContainer = append(dataContainer, record)
+	}
+	return dataContainer
+}
+
+// recordedPrice functions END
 
 func main() {
-	createCsvReader(BTRX)
+	histDataReader := createCsvReader(HistDataBTC)
+	histData, err := histDataReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	output := processHistData(histData)
+	fmt.Println(output)
 }
