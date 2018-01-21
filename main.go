c63aa58cb8a23f58f2ef7655d926b7b000da58fa
Frank Meza <frankmezapdx@gmail.com>
2018-01-21 13:42:24 -0800

    add possible new fn and struct

--- main.go
+++ main.go
@@ -5,16 +5,23 @@ import (
 	"strings"
 )
 
+type recordedPrice struct {
+}
+
 func bittrexCSVReader() *csv.Reader {
 	btrxCsv := strings.NewReader(BTRX)
 	r := csv.NewReader(btrxCsv)
 	return r
 }
 
-func histDataBTCCSVReader() *csv.Reader {
+func histDataBtcCSVReader() *csv.Reader {
 	histDataBtcCsv := strings.NewReader(HistDataBTC)
 	r := csv.NewReader(histDataBtcCsv)
 	return r
+}
+
+// process getHistDataLowWithDate :: []string -> RecordedPrice{}
+func getHistDataLowWithDate(histData []string) recordedPrice {
 }
 
 func main() {
