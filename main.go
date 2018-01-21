8a189e2374b4b23b8932c2375efd7de949e9dc2d
Frank Meza <frankmezapdx@gmail.com>
2018-01-21 13:54:39 -0800

    refactor into one function

--- main.go
+++ main.go
@@ -5,25 +5,20 @@ import (
 	"strings"
 )
 
-type recordedPrice struct {
-}
+// type recordedPrice struct {
+// }
 
-func bittrexCSVReader() *csv.Reader {
-	btrxCsv := strings.NewReader(BTRX)
-	r := csv.NewReader(btrxCsv)
-	return r
-}
-
-func histDataBtcCSVReader() *csv.Reader {
-	histDataBtcCsv := strings.NewReader(HistDataBTC)
-	r := csv.NewReader(histDataBtcCsv)
+// createCsvReader for use with BTRX, HistDataBTC
+func createCsvReader(csvDataString string) *csv.Reader {
+	data := strings.NewReader(csvDataString)
+	r := csv.NewReader(data)
 	return r
 }
 
 // process getHistDataLowWithDate :: []string -> RecordedPrice{}
-func getHistDataLowWithDate(histData []string) recordedPrice {
-}
+// func getHistDataLowWithDate(histData []string) recordedPrice {
+// }
 
 func main() {
-
+	createCsvReader(BTRX)
 }
